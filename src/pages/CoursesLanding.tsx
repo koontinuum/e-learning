98d815e5485a9fa92eec506f4fad8fe74e8afc44
meljum <melisjumabek58@gmail.com>
2023-05-24 23:21:01 +0600

    add feat/ourTeacher OurTeachers CardTeachers

--- src/pages/CoursesLanding.tsx
+++ src/pages/CoursesLanding.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import Header from "../components/header/Header";
 import InterComp from "../components/interComp/InterComp";
+import OurTeachers from "../components/ourTeachers/OurTeachers";
 
 function CoursesLanding() {
   return (
     <div>
       <Header />
+      <OurTeachers/>
       <InterComp/>
-    </div>
+      </div>
   );
+    
 }
 
 export default CoursesLanding;
