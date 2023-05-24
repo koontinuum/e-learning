ab15d9b8e3a0fbb1ff307933eb5ad8e382521c7a
Saadatov <saadatovbekmyrza59@gmail.com>
2023-05-24 16:01:13 +0600

    feat/brands

---
+++ src/components/DownloadApp/DownloadApp.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import scss from 'Download.module.scss'
+function DownloadApp() {
+  return (
+    <div className={scss.download}>
+        <div>
+            <h2>Download App</h2>
+        </div>
+    </div>
+  )
+}
+
+export default DownloadApp
--- src/pages/CoursesLanding.tsx
+++ src/pages/CoursesLanding.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Header from "../components/header/Header";
+import WeWorkWith from "../components/WeWorkWith/WeWorkWith";
+import DownloadApp from "../components/DownloadApp/DownloadApp";
 
 function CoursesLanding() {
   return (
     <div>
       <Header />
+      <WeWorkWith />
+      <DownloadApp/>
     </div>
   );
 }
