6bf6dacede4c6f62d2ba3198e949e3c210f60001
Saadatov <saadatovbekmyrza59@gmail.com>
2023-05-24 16:30:32 +0600

    feat/brands

---
+++ src/components/WeWorkWith/WeWorkWith.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import scss from './WeWorkWith.module.scss'
+import { WeWorkWithData } from '../../objects/WeWorkWith/WeWorkWithData'
+import spotify from '../../images/WeWorkWith/brand_spotify.svg'
+import slack from '../../images/WeWorkWith/stack.svg'
+
+function WeWorkWith() {
+  return (
+    <div className={scss.wrapper__brands}>
+        <div>
+            <h2>We Work With</h2>
+    <p>Quisque aliquet, libero consequat elementum convallis.</p>
+    </div>
+    <div className={scss.brands}> 
+    {
+        WeWorkWithData.map(item => (
+            <div key={item.id}>
+                <img src={item.img} alt="" />
+            </div>
+        ))
+    }
+    <div className={scss.adaptive}>
+        <img src={spotify} alt="" />
+        <img src={slack} alt="" />
+    </div>
+    </div>
+    
+    </div>
+  )
+}
+
+export default WeWorkWith
--- src/pages/CoursesLanding.tsx
+++ src/pages/CoursesLanding.tsx
@@ -3,14 +3,17 @@ import Header from "../components/header/Header";
 import FeaturedCategory from "../components/FeaturedCategory/FeaturedCategory";
 import Hero from "../components/Hero/Hero";
 import InterComp from "../components/interComp/InterComp";
+import WeWorkWith from "../components/WeWorkWith/WeWorkWith";
 
 function CoursesLanding() {
   return (
     <div>
       <Header />
       <Hero/>
+      <WeWorkWith/>
       <FeaturedCategory/>
       <InterComp/>
+
     </div>
   );
 }
